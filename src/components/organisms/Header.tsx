94a0b41256df8be597eb9d79c0cae8001d06d668
taixnishi <taichi.1999.baske@gmail.com>
2022-11-09 01:10:33 +0900

    headerにformを追加

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -7,9 +7,26 @@ import {
   useColorMode,
   Spacer,
 } from "@chakra-ui/react";
-import TodoForm from "components/organisms/TodoForm";
+import TodoForm from "components/organisms/TodoInput";
+import { useAppDispatch, useAppSelector } from "hooks/storeHooks";
+import { FormProvider, useForm } from "react-hook-form";
+import { addTodo, clearTodoList } from "store/slices/todoReducer";
+import { TodoType } from "types";
+
 const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const methods = useForm();
+  const todoList = useAppSelector((state) => state.todo.todoList);
+  const dispatch = useAppDispatch();
+  function onSubmit(values: any) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const newTodo: TodoType = { ...values, id: todoList.length + 1 , isChecked: false};
+        dispatch(addTodo(newTodo));
+        methods.reset();
+        resolve("ok");
+      }, 500);
+    });
+  }
   return (
     <header>
       <Box bg="teal" p={3} h="300px">
@@ -17,16 +34,26 @@ const Header = () => {
           <Heading as="h1" my={10}>
             Todo App
           </Heading>
-          <TodoForm />
-          <Flex>
-            <Button onClick={toggleColorMode} mt={5}>
-              Toggle {colorMode === "light" ? "Dark" : "Light"}
-            </Button>
-            <Spacer />
-            <Button mt={5} colorScheme="telegram">
-              追加
-            </Button>
-          </Flex>
+          {/* form */}
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)}>
+              <TodoForm />
+              <Flex>
+                {/* <Button onClick={toggleColorMode} mt={5}>
+                  Toggle {colorMode === "light" ? "Dark" : "Light"}
+                </Button> */}
+                <Spacer />
+                <Button
+                  mt={5}
+                  colorScheme="telegram"
+                  isLoading={methods.formState.isSubmitting}
+                  type="submit"
+                >
+                  追加
+                </Button>
+              </Flex>
+            </form>
+          </FormProvider>
         </Container>
       </Box>
     </header>
